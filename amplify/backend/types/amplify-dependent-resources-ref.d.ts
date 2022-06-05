d990d6912e84bc137ce9513cc878cbd88f8c8746
Yonata Jaraba <ygjr_123@hotmail.com>
2022-06-05 14:09:56 -0500

    Subiendo tercer commit

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "parcialbuenaspractic": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
